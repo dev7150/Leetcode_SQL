7d8859b2d7da01a681edd73820cc760465a54a02
Dev P <devashish16@gmail.com>
2022-11-01 14:15:15 +1100

    Initial Commit

--- SQL.sql
+++ SQL.sql
@@ -7,4 +7,13 @@ from
     World
 where
     area >= 3000000
-    or population >= 25000000
+    or population >= 25000000;
+    
+-- 1757. Recyclable and Low Fat Products
+Select
+    product_id
+from
+    Products
+where
+    low_fats = 'Y'
+    and recyclable = 'Y';
